eec0436f3ec2c379340f8e2cfd75658ebf03eb0d
Richard Crooks <email@rocrooks.co.uk>
2018-12-05 20:25:31 +0000

    Add generate a random digit function

--- functions/maths-functions.php
+++ functions/maths-functions.php
@@ -16,4 +16,36 @@ function normalisevalue($value,$min,$max)
   Return $normalised;
   }
 //---FunctionBreak---
+/*Generates a random digit between 0 and 1
+
+$edges is default to true and can generate 0 or 1s
+$maximum is the range to generate random numbers across. This is defaulted to false and will be generated automatically, set if the function will be called repeatedly
+
+Output is a decimal between 0 and 1*/
+//---DocumentationBreak---
+function randomdigit($edges=true,$maximum=false)
+  {
+  //Generate maximum if not already generated
+  if ($maximum == false)
+    $maximum = mt_getrandmax();
+
+  $number = mt_rand(0,$maximum);
+
+  //Use edge property to define what to normalise against
+  if ($edges == true)
+    {
+    $normmin = 0;
+    $normmax = $maximum;
+    }
+  else
+    {
+    $normmin = -1;
+    $normmax = $maximum+1;
+    }
+
+  //Normalise and return
+  $number = normalisevalue($number,$normmin,$normmax);
+  Return $number;
+  }
+//---FunctionBreak---
 ?>
--- public/create-distribution.php
+++ public/create-distribution.php
@@ -1,4 +1,6 @@
 <?php
+include_once '../functions/maths-functions.php';
+
 //Define the terms by default if they're not already specified
 if (isset($mean) == false)
   $mean = 0;
@@ -7,6 +9,11 @@ if (isset($sd) == false)
 if (isset($values) == false)
   $values = 10;
 
+//Limit the maximum possible range for generating random numbers to 2147483647 due to array size
 $maximum = mt_getrandmax();
-echo $maximum;
+if ($maximum > 2147483647)
+  $maximum = 2147483647;
+
+//Retrieve random digit
+echo randomdigit(false,$maximum);
 ?>
